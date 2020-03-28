f04faccbea1f410a91908673ae4fa3ce82ecd171
Sumon Ahmed <bdsumon4u@gmail.com>
2020-03-28 09:39:39 +0600

    Guard Based Logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,17 +54,21 @@
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                                @foreach(config('auth.guards') as $guard => $data)
+                                    @auth($guard)
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route($guard == 'web' || $guard == null ? 'logout' : $guard . '.logout') }}"
+                                        onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
+                                        <form id="logout-form" action="{{ route($guard == 'web' || $guard == null ? 'logout' : $guard . '.logout') }}" method="POST" style="display: none;">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                    @endauth
+                                @endforeach
                             </li>
                         @endguest
                     </ul>
